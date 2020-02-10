264c40c7adbf5d75400493bbc0f35c17064b04dd
laurentiu-meghesan <laurentiu.meghesan@gmail.com>
2020-02-10 21:33:49 +0200

    displaying contacts in console

--- js/contacts.js
+++ js/contacts.js
@@ -1,11 +1,26 @@
 window.PhoneBook = {
 
-    API_BASE_URL: "http://localhost:8082/",
+    API_BASE_URL: "http://localhost:8082/agenda",
 
     getContacts: function () {
+        $.ajax({
+            url:PhoneBook.API_BASE_URL,
+            method: "GET"
+        }).done(function (response) {
+            console.log(response);
+        })
+    },
 
-        console.log()
-        
+    getContactRow:function (contact) {
+        return `<tr>
+            <td>${contact.firstName}</td>
+            <td>${contact.lastName}</td>
+            <td>${contact.phoneNumber}</td>
+            <td><a href="#" data-id=${contact.id} class="delete-contact"><button>Delete</button></a></td>
+
+        </tr>`
     }
 
 };
+
+PhoneBook.getContacts();
