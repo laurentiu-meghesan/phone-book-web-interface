3d53b9f07f72243852726ce784e7a1d4794bc675
laurentiu-meghesan <laurentiu.meghesan@gmail.com>
2020-02-15 02:28:13 +0200

    added create and delete contact methods

--- js/contacts.js
+++ js/contacts.js
@@ -4,23 +4,93 @@ window.PhoneBook = {
 
     getContacts: function () {
         $.ajax({
-            url:PhoneBook.API_BASE_URL,
+            url: PhoneBook.API_BASE_URL,
             method: "GET"
         }).done(function (response) {
             console.log(response);
+            PhoneBook.displayContacts(JSON.parse(response));
         })
     },
 
-    getContactRow:function (contact) {
+    deleteContact: function (id) {
+        $.ajax({
+            url: PhoneBook.API_BASE_URL + "?id=" + id,
+            method: "DELETE"
+        }).done(function () {
+            PhoneBook.getContacts();
+        })
+    },
+
+    createContact: function () {
+
+        let firstNameValue = $("#firstName-field").val();
+        let lastNameValue = $("#lastName-field").val();
+        let phoneNumberValue = $("#phoneNumber-field").val();
+
+        let requestBody = {
+            firstName: firstNameValue,
+            lastName: lastNameValue,
+            phoneNumber: phoneNumberValue
+        };
+
+        $.ajax({
+            url: PhoneBook.API_BASE_URL,
+            method: "POST",
+            contentType: "application/json",
+            data: JSON.stringify(requestBody)
+        }).done(function () {
+            PhoneBook.getContacts();
+        })
+    },
+
+    updateContact: function(id){
+
+        $.ajax({
+            url: PhoneBook.API_BASE_URL + "?id=" + id,
+            method: "PUT",
+            contentType: "application/json",
+
+        }).done(function () {
+            PhoneBook.getContacts()
+        })
+    },
+
+    getContactRow: function (contact) {
+
         return `<tr>
             <td>${contact.firstName}</td>
             <td>${contact.lastName}</td>
             <td>${contact.phoneNumber}</td>
-            <td><a href="#" data-id=${contact.id} class="delete-contact"><button>Delete</button></a></td>
-
+            <td><a href="#" data-id=${contact.id} class="delete-contact"><i class="fas fa-user-slash"></i></a></td>
         </tr>`
-    }
+    },
 
+    displayContacts: function (contacts) {
+        var tableBody = '';
+
+        contacts.forEach(contact => tableBody += PhoneBook.getContactRow(contact))
+
+        $("#contacts-table tbody").html(tableBody);
+    },
+
+    bindEvents: function () {
+
+        $("#new-contact-form").submit(function (event) {
+            event.preventDefault();
+
+            PhoneBook.createContact();
+        });
+
+
+        $("#contacts-table").delegate(".delete-contact", "click", function (event) {
+            event.preventDefault();
+            let contactId = $(this).data("id");
+
+            PhoneBook.deleteContact(contactId);
+        })
+
+    }
 };
 
 PhoneBook.getContacts();
+PhoneBook.bindEvents();
